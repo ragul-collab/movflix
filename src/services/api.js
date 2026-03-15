27fa014521043d6b06797f1e60487ea9915faf65
ragul-collab <raguldharani1703@gmail.com>
2026-03-15 22:25:57 +0530

    updated api key

--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,22 @@
-const API_KEY = import.meta.env.VITE_API_KEY
+const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN
 const BASE_URL = "https://api.themoviedb.org/3";
 
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${ACCESS_TOKEN}`
+    }
+}
+
 export const getPopularMovies = async () => {
-    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const response = await fetch(`${BASE_URL}/movie/popular`, options)
     const data = await response.json()
     return data.results
 };
 
 export const searchMovies = async (query) => {
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
-    const data = await response.json();
-    return data.results;
-};
+    const response = await fetch(`${BASE_URL}/search/movie?query=${encodeURIComponent(query)}`, options)
+    const data = await response.json()
+    return data.results
+}
